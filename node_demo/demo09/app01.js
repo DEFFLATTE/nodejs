f499387bbabd10fff7a4cf1e0ed375715ff92c8a
zhou <1020812251@qq.com>
2020-07-27 11:54:53 +0800

    1

---
+++ node_demo/demo09/app01.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+http.createServer(function (req, res) {
+    // http://127.0.0.1:8081/login.html
+    // http://127.0.0.1:8081/index.html
+
+    // 1.获取地址
+
+    let pathname = req.url;
+    // console.log(pathname);
+    pathname=pathname=='/'?'/index.html':pathname;
+    console.log(pathname);
+    // 2.通过fs模块读取文件
+    if(pathname!='/favicon.ico'){
+        fs.readFile('./static'+pathname,(err,data)=>{
+            if(err){
+                res.writeHead(404, {'Content-Type': 'text/html;charset="utf-8'});
+                res.end('这个页面不存在');
+            }
+            res.writeHead(200, {'Content-Type': 'text/html;charset="utf-8'});
+            res.end(data);
+        })
+    }
+
+   
+}).listen(8888);
+
+console.log('Server running at http://127.0.0.1:8888/');
